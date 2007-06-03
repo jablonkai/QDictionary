e8d63b43718b350720649df55dff39f07ce4ae83
tamas.jablonkai <tamas.jablonkai@69855f12-d931-0410-a904-216131717e12>
2007-06-03 17:34:03 +0000

    git-svn-id: http://qdictionary.googlecode.com/svn/trunk@11 69855f12-d931-0410-a904-216131717e12

---
+++ src/translations/qdictionary_hu.ts
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="hu">
+<context>
+    <name>DictionaryTree</name>
+    <message>
+        <location filename="../dictionarytree.cpp" line="22"/>
+        <source>Dictionary</source>
+        <translation>Szótár</translation>
+    </message>
+    <message>
+        <location filename="../dictionarytree.cpp" line="45"/>
+        <source>%1 dictionaries loaded</source>
+        <translation>%1 szótár betöltve</translation>
+    </message>
+</context>
+<context>
+    <name>DictionaryWidget</name>
+    <message>
+        <location filename="../ui/dictionarywidget.ui" line="13"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dictionarywidget.cpp" line="42"/>
+        <source>The number of results: %1<byte value="x9"/>(Within %2 sec)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="13"/>
+        <source>QDictionary</source>
+        <translation>QDictionary</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="27"/>
+        <source>&amp;File</source>
+        <translation>&amp;Fájl</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="36"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="51"/>
+        <source>Dictionaries</source>
+        <translation>Szótárak</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="74"/>
+        <source>1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="84"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Kilépés</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="89"/>
+        <source>About</source>
+        <translation>Névjegy</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="94"/>
+        <source>About Qt</source>
+        <translation>Qt névjegye</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="39"/>
+        <source>About QDictionary</source>
+        <translation type="unfinished">QDictionary névjegye</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../dictionary.cpp" line="21"/>
+        <source>QVocabulary</source>
+        <translation>QVocabulary</translation>
+    </message>
+    <message>
+        <location filename="../dictionary.cpp" line="21"/>
+        <source>Cannot read file %1:
+%2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dictionaryreader.cpp" line="24"/>
+        <source>The file is not an QVOC file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
